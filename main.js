59ca32fff5d39a0fceb45e4ab5a247d708455c4a
c <thomascavalcante77@gmail.com>
2023-01-11 17:55:47 -0500

    ui change test

---
+++ main.js
@@ -0,0 +1,15 @@
+
+const data = document.querySelector('#textData');
+const qrCode = document.querySelector('#qrCode');
+const qrGenerator = document.querySelector('#qrGenerator');
+
+const baseURL = "https://api.qrserver.com/v1/create-qr-code/"
+
+qrGenerator.addEventListener('click',()=>{
+    const size = `350x350`
+    qrCode.src = `${baseURL}?/size=${size}&data=${data.value}`
+
+    if (data.value == "") {
+        qrCode.src = "/asset/QRdefault.png"
+    }
+})
