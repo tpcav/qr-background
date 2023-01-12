255c3253ac2657a341fadbb84aa26bb377c4889d
c <thomascavalcante77@gmail.com>
2023-01-12 12:17:12 -0500

    ui change test

--- main.js
+++ main.js
@@ -3,9 +3,10 @@ const qrCode = document.querySelector('#qrCode');
 const qrGenerator = document.querySelector('#qrGenerator');
 const qrBackground = document.querySelector('.qrBackground');
 const saveButton = document.querySelector('#saveButton');
-const baseURL = "https://api.qrserver.com/v1/create-qr-code/";
 const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+
+
+const baseURL = "https://api.qrserver.com/v1/create-qr-code/";
 
 qrGenerator.addEventListener('click',()=>{
   const size = `350x350`
@@ -17,8 +18,6 @@ qrGenerator.addEventListener('click',()=>{
   } else {
       saveButton.disabled = false;
   }
-  // Add class to show QR code
-  qrCode.classList.add("show-on-save");
 })
 
 function randomBackground() {
@@ -39,47 +38,4 @@ function getRandomColor() {
 
 document.querySelector("#randColor").addEventListener("click", randomBackground);
 saveButton.addEventListener("click", saveImage);
-
-function saveImage() {
-    // Set canvas size to match QR code background
-    canvas.width = qrBackground.clientWidth;
-    canvas.height = qrBackground.clientHeight;
-
-    // Draw QR code background image on canvas
-    ctx.drawImage(qrBackground, 10, 10);
-
-    // Draw QR code image on canvas
-    ctx.drawImage(qrCode);
-
-    // Save canvas as PNG image
-    const image = canvas.toDataURL("image/png");
-    const link = document.createElement("a");
-    link.href = image;
-    link.download = "qr-code-background.png";
-    link.click();
-}
-
-function savePageAsImage() {
-  // hide elements
-  document.querySelectorAll('.container, .submitButtons:not(.show-on-save)').forEach(function(el) {
-    el.classList.add('hide-on-save');
-  });
-  // delay to make sure the qrCode is fully rendered
-  setTimeout(() => {
-      // Capture the current state of the page
-      html2canvas(document.body, {scale: 2}).then(function(canvas) {
-          // Convert the canvas to a PNG image
-          var imgData = canvas.toDataURL('image/png', 1);
-          // Show the elements again
-          document.querySelectorAll('.container, .submitButtons:not(.show-on-save)').forEach(function(el) {
-            el.classList.remove('hide-on-save');
-          });
-          // Save the image to the user's device
-          var link = document.createElement('a');
-          link.download = 'QR-Background.png';
-          link.href = imgData;
-          link.click();
-      });
-  }, 500);
-}
 
